f27cc39d872adaa72852fa9279484f5a1bfe74e2
SonVuPDT <vuson2002020@gmail.com>
2024-01-03 10:32:43 +0700

    init project

--- Xuly.js
+++ Xuly.js
@@ -25,7 +25,7 @@ function start() {
         body:JSON.stringify(formdata)
         
     };
-  console.log(options.body);
+  
     fetch(getAPI,options)
     .then(function(response)
     {
@@ -38,16 +38,16 @@ function start() {
     var body = document.querySelector("tbody");
     var html = student.map(function (sv) {
         return `<tr>
-        <td>${sv.ID}</td>
-        <td>${sv.Name}</td>
-        <td>${sv.Age}</td>
-        <td>${sv.Address}</td>
-        <td>${sv.Ngaysinh}</td>
-        <td>${sv.Hinhanh}</td>
-        <td>${sv.Ngaygio}</td>
+        <td id="id-${sv.id}">${sv.id}</td>
+        <td id="name-${sv.id}">${sv.Name}</td>
+        <td id="age-${sv.id}">${sv.Age}</td>
+        <td id="address-${sv.id}">${sv.Address}</td>
+        <td id="ngaysinh-${sv.id}">${sv.Ngaysinh}</td>
+        <td id="hinhanh-${sv.id}"><img src="${sv.Hinhanh}"></td>
+        <td id="ngaygio-${sv.id}">${sv.Ngaygio}</td>
         <td>
-            <button onclick=deletestudent(${sv.ID})>Xóa</button>
-            <button onclick=hienthi_update(${sv.ID}) >Sửa</button>
+            <button onclick=deletestudent(${sv.id})>Xóa</button>
+            <button onclick=hienthi_update(${sv.id}) >Sửa</button>
           </td>
         </tr>`;
     });
@@ -59,6 +59,7 @@ function start() {
     var age=document.querySelector('input[name="Age"]').value;
     var address=document.querySelector('input[name="Address"]').value;
     var date=document.querySelector('input[name="Ngaysinh"]').value;
+    var img=document.querySelector('input[name="Hinhanh"]').value;
     var datetime=document.querySelector('input[name="Ngaygio"]').value;
     var ns=new Date(date);
    var ngsinh=ns.toLocaleDateString("vi-VN");
@@ -125,7 +126,7 @@ function deletestudent(id)
         'Content-Type':'application/json'
     },
 };
-console.log(options.body);
+
 fetch(getAPI+'/'+id,options)
 .then(function(response)
 {
@@ -138,4 +139,69 @@ fetch(getAPI+'/'+id,options)
   });
 });
 
+}
+
+// var Ns=new Date(ddate);
+// var ngsinh=ns.toLocaleDateString("vi-VN");
+// var ng=new Date(datetime);
+// var ngaygio=ng.toLocaleDateString("vi-VN");
+
+var Name=document.getElementById("Name");
+var Age=document.getElementById("Age");
+var Address=document.getElementById("Address");
+var ddate=document.getElementById("Ngaysinh");
+var Img=document.getElementById("Hinhanh");
+var Datetime=document.getElementById("Ngaygio");
+function hienthi_update(id)
+{
+  console.log(id);
+  var name=document.getElementById("name-"+id).innerText;
+  var age=document.getElementById("age-"+id).innerText;
+  var address=document.getElementById("address-"+id).innerText;
+  var date=document.getElementById("ngaysinh-"+id).innerText;
+  var hinhanh=document.getElementById("hinhanh-"+id).innerText;
+
+  var ngaygio=document.getElementById("ngaygio-"+id).innerText;
+console.log(name);
+ 
+  /////
+ 
+  Name.value=name;
+  Age.value=age;
+  Address.value=address;
+  ddate.value=date;
+  Datetime.value=ngaygio;
+  var btnSua=document.getElementById("buttonSua");
+  
+  btnSua.setAttribute('onclick',`update(${id})`)
+}
+function update(id)
+{
+  var data={
+    Name:Name.value,
+    Age:Age.value,
+    Address:Address.value,
+    Ngaysinh:ddate.value,
+    Hinhanh:parseImage,
+    Ngaygio:Datetime.value
+  }
+  var options={
+    method:'PUT',
+    headers:{
+        'Content-Type':'application/json'
+    },
+    body:JSON.stringify(data)
+};
+
+fetch(getAPI+'/'+id,options)
+.then(function(response)
+{
+    response.json();
+}
+)
+.then(function(){
+  getStudent(function (sinhvien) {
+    displaystudent(sinhvien);
+  });
+});
 }
